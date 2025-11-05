5f44d4e09fe010ae311b1ef567f49be793313557
Tony-Android <anthony09141991@gmail.com>
2025-11-05 11:14:24 -0600

    Commit app.py

--- test_text_scooter_after_click.py
+++ test_text_scooter_after_click.py
@@ -5,15 +5,15 @@ from urban_routes_main_page import UrbanRoutesPage  # Import the POM class
 def test_custom_scooter_option():
     # Step 1: Open the app - update the URL after starting the server
     driver = webdriver.Chrome()
-    driver.get('https://cnt-52982b89-e8ef-46f8-ab2b-2a790d72d1c1.containerhub.tripleten-services.com/')
+    driver.get('https://cnt-a56ab7f9-a8e6-424a-afe7-75f8f2ae1364.containerhub.tripleten-services.com/')
 
     # Create an instance of the page class
     urban_routes_page = UrbanRoutesPage(driver)
 
     # Step 3: Use POM methods to perform actions on the page
     # Enter "From" and "To" locations.
-    urban_routes_page.enter_from_location('East 2nd Street, 601')
-    urban_routes_page.enter_to_location('1300 1st St')
+    # Use the "enter_locations" step to enter both locations
+    urban_routes_page.enter_locations('East 2nd Street, 601', '1300 1st St')
 
     # Select the "Custom" option.
     urban_routes_page.click_custom_option()
@@ -23,7 +23,8 @@ def test_custom_scooter_option():
     urban_routes_page.click_scooter_icon()
     time.sleep(2)  # Adding delay for visibility; optional
 
-    # Step 4: Verify the Scooter text is displayed correctly
-    actual_value = urban_routes_page.get_scooter_text()
-    expected_value = "Scooter"
+    actual_value = urban_routes_page.get_duration_text()
+    expected_value = "Duration"
     assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+    driver.quit()
--- urban_routes_main_page.py
+++ urban_routes_main_page.py
@@ -9,6 +9,8 @@ class UrbanRoutesPage:
     CUSTOM_OPTION_LOCATOR = (By.XPATH, '//div[text()="Custom"]')
     SCOOTER_ICON_LOCATOR = (By.XPATH, '//img[@src="/static/media/scooter.cf9bb57e.svg"]')
     SCOOTER_TEXT_LOCATOR = (By.XPATH, '//div[@class="results-text"]//div[@class="text"]')
+    # The new "Duration" text locator
+    DURATION_TEXT_LOCATOR = (By.XPATH, '//div[@class="results-text"]//div[@class="duration"]')
 
     def __init__(self, driver):
         self.driver = driver  # Initialize the driver
@@ -32,3 +34,11 @@ class UrbanRoutesPage:
     def get_scooter_text(self):
         # Return the "Scooter" text
         return self.driver.find_element(*self.SCOOTER_TEXT_LOCATOR).text
+
+    def get_duration_text(self):
+        return self.driver.find_element(*self.DURATION_TEXT_LOCATOR).text
+
+        # Step to enter both "From" and "To" location
+    def enter_locations(self, from_text, to_text):
+        self.enter_from_location(from_text)
+        self.enter_to_location(to_text)
